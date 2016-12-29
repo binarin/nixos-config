2c12b11825ebd5740b1f042efa5f7510689b6449
Alexey Lebedeff <alebedev@mirantis.com>
2016-12-29 20:50:17 +0300

    Add lanfear config

---
+++ partitions/unencrypted-uefi-ext4.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Use the gummiboot efi boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos-root";
+    fsType = "ext4";
+    options = [ "noatime" "discard" ];
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-partlabel/uefi-boot";
+    fsType = "vfat";
+  };
+
+  swapDevices = [ {device = "/dev/disk/by-label/swap-0"; } ];
+}
