0127f2009f85237db977f654b048fe56da9f3735
Alexey Lebedeff <binarin@binarin.ru>
2017-03-20 19:38:48 +0300

    Bump bleeding edge

--- packages/bleeding-edge.nix
+++ packages/bleeding-edge.nix
@@ -4,9 +4,9 @@ let
   master-nixpkgs-checkout = pkgs.fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    name = "nixpkgs-master-2017-02-12";
-    rev = "31eba21d1dab7da2e75be229c542990e8f4b56b6";
-    sha256 = "1mqx7c5jwgnka8zb83hq7wb955qy4jb2mk44b3wvan8w1a0sqc88";
+    name = "nixpkgs-master-2017-03-20";
+    rev = "20194e2696a276937bfe7b448951036831c69700";
+    sha256 = "1a2g3xkcms7jbp42a57nsyrhd9x5l14y4ycpars7b0sir5896jx9";
   };
  # master-nixpkgs-checkout = /home/binarin/personal-workspace/nixpkgs;
  master-nixpkgs = import master-nixpkgs-checkout {inherit (config.nixpkgs) config;};
