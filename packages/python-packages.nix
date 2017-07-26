33894c506a37789cd1d705d221493482f8bebdbd
Alexey Lebedeff <alebedev@mirantis.com>
2017-07-26 14:23:39 +0300

    Add jupyter to QSTK python env

--- packages/python-packages.nix
+++ packages/python-packages.nix
@@ -1,4 +1,4 @@
-{pkgs, fetchurl, ...}:
+{pkgs, stdenv, ...}:
 
 let
   packageOverrides = self: super: {
@@ -6,28 +6,30 @@ let
       name = "QSTK-${version}";
       version = "0.2.8";
       propagatedBuildInputs = with self; [ dateutil numpy scipy matplotlib pandas cvxopt scikitlearn ];
-      src = pkgs.fetchurl {
-        name = "QSTK-0.2.8.tar.gz";
-        url = "https://pypi.python.org/packages/8c/15/3298c4ee5d187a462883a7f80d7621a05e8b880a8234729e733769a3476f/QSTK-0.2.8.tar.gz#md5=bac816724bcb6eebd5dbb09e43166815";
-        sha256 = "1h88m8ahk2c0mqgclryax0sw9wd3lgfx0vly002chw60xb0zvbrz";
-      };
-    };
-    dateutil = super.buildPythonPackage {
-      name = "python-dateutil-1.5";
-      src = pkgs.fetchurl {
-        url = "https://pypi.python.org/packages/b4/7c/df59c89a753eb33c7c44e1dd42de0e9bc2ccdd5a4d576e0bfad97cc280cb/python-dateutil-1.5.tar.gz";
-        sha256 = "02dhw57jf5kjcp7ng1if7vdrbnlpb9yjmz7wygwwvf3gni4766bg";
+      src = pkgs.fetchFromGitHub {
+        owner = "binarin";
+        repo = "QuantSoftwareToolkit";
+        rev = "a48a80465840829cc1b294e73433eecf35fdd9be";
+        sha256 = "1q7n3gr1inm86q9xb5g8mm1gzn6k8rcm7sk0qy8631lk4a66nqbb";
       };
     };
   };
+  python = pkgs.python27.override {inherit packageOverrides; };
+  packagesToInstall = ps: with ps; [
+    QSTK
+    ipython
+    jupyter
+  ];
 in {
   options = {};
   config = {
     environment.systemPackages = [ pkgs.myPython27 ];
     nixpkgs.config.packageOverrides = super: rec {
-      myPython27 = (pkgs.python27.override {inherit packageOverrides;}).withPackages (ps: with ps; [
-        QSTK
-      ]);
+      myPython27 = python.buildEnv.override {
+        extraLibs = packagesToInstall python.passthru.pkgs;
+        ignoreCollisions = true;
+      };
     };
   };
 }
+
