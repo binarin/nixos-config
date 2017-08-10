f61b72e13132e70e200788e50f3fb29ef6a3601b
Alexey Lebedeff <alebedev@mirantis.com>
2017-08-10 17:25:24 +0300

    Add balthamel config

---
+++ partitions/laptop-crypt.nix
@@ -0,0 +1,21 @@
+{...}:
+
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = [ { name = "laptop-crypt"; device="/dev/disk/by-partlabel/laptop-crypt"; allowDiscards = true; } ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos-root ";
+      fsType = "ext4";
+      options = [ "noatime" "discard" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-partlabel/nixos-boot";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ {device = "/dev/disk/by-label/swap-0"; } ];
+}
