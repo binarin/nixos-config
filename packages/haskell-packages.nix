9724cd3ccadffd12d4f7f642ae76a576606033a2
Alexey Lebedeff <alebedev@mirantis.com>
2017-08-27 19:16:53 +0200

    Trying to make GHC 8.2.1 work
    ghc-mod is broken

--- packages/haskell-packages.nix
+++ packages/haskell-packages.nix
@@ -5,14 +5,35 @@
   config = {
     environment.systemPackages = [
       pkgs.ghc80Env
+      # pkgs.ghc82Env
       pkgs.ghc80ProfEnv
     ];
     nixpkgs.config.packageOverrides = super: rec {
-      haskellPackages = super.haskellPackages.override {
-        overrides = self: super: {
-          ghc-syb-utils = pkgs.haskell.lib.dontCheck super.ghc-syb-utils;
+      # haskellPackages = super.haskellPackages.override {
+      #   overrides = self: super: {
+      #     ghc-syb-utils = pkgs.haskell.lib.dontCheck super.ghc-syb-utils;
+      #   };
+      # };
+      myHaskell82Packages = libProf: self: super:
+        with pkgs.haskell.lib;
+        let pkg = self.callPackage; in
+        let reflex-src = import ./haskell-reflex-frp.nix {inherit (pkgs) fetchFromGitHub; }; in
+        rec {
+          # clay = dontCheck super.clay;
+          # ghc-syb-utils = dontCheck super.ghc-syb-utils;
+          # encoding = appendPatch super.encoding ./haskell-encoding.patch; # remove constraint on 'binary' so it'll build at all; and ignore GB18030 as it's built time is around an hour
+          quickcheck-instances = doJailbreak super.quickcheck-instances;
+          # reflex = pkg reflex-src.reflex {};
+          mkDerivation = args: super.mkDerivation (args // {
+            enableLibraryProfiling = libProf;
+            enableExecutableProfiling = false;
+          });
         };
+
+      haskell821Packages = pkgs.bleeding.haskell.packages.ghc821.override {
+        overrides = myHaskell82Packages false;
       };
+
       haskell802Packages = super.haskell.packages.ghc802.override {
         overrides = myHaskellPackages false;
       };
@@ -60,6 +81,24 @@
           cabal2nix
           ghc-core
           ghc-mod
+          hasktags
+          happy
+          hlint
+          hpack
+          stylish-haskell
+        ];
+      };
+
+      ghc82Env = super.pkgs.myEnvFun {
+        name = "ghc82";
+        buildInputs = with haskell821Packages; [
+          (ghcWithHoogle (p: []))
+          hspec
+          alex
+          cabal-install
+          # cabal2nix
+          ghc-core
+          # ghc-mod
           hasktags
           happy
           hlint
