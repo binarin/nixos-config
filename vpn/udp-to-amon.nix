6e87a3af9bc34f68d1b0cc929ea989f8d6e21d5b
Alexey Lebedeff <binarin@binarin.ru>
2018-01-12 18:15:42 +0100

    Fix amon vpn config

--- vpn/udp-to-amon.nix
+++ vpn/udp-to-amon.nix
@@ -4,7 +4,7 @@ pkgs.lib.mkIf (builtins.pathExists "/root/.openvpn/${config.networking.hostName}
   config = ''
     client
     cipher AES-128-CBC
-    dev ovpn-naberius
+    dev ovpn-amon
     dev-type tun
     proto udp
     remote amon.binarin.ru 2149
@@ -14,9 +14,9 @@ pkgs.lib.mkIf (builtins.pathExists "/root/.openvpn/${config.networking.hostName}
     group nogroup
     persist-key
     persist-tun
-    ca /root/.openvpn/${config.networking.hostName}-to-naberius-udp-server-ca.crt
-    cert /root/.openvpn/${config.networking.hostName}-to-naberius-udp-server.crt
-    key /root/.openvpn/${config.networking.hostName}-to-naberius-udp-server.key
+    ca /root/.openvpn/${config.networking.hostName}-to-amon-udp-server-ca.crt
+    cert /root/.openvpn/${config.networking.hostName}-to-amon-udp-server.crt
+    key /root/.openvpn/${config.networking.hostName}-to-amon-udp-server.key
     ns-cert-type server
     comp-lzo
     verb 3
