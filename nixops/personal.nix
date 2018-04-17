8a14e33bf10e7963820bb53acde8f7cb7d0bbf3a
Alexey Lebedeff <binarin@binarin.ru>
2018-04-17 09:05:44 +0200

    Start to use nixops deployment keys

--- nixops/personal.nix
+++ nixops/personal.nix
@@ -9,5 +9,20 @@
      imports = [
        ../configuration.nix-amon
      ];
+     deployments.keys.hass-http-password = {
+       text = builtins.getEnv "DEPLOY_HASS_HTTP_PASSWORD";
+       user = "hass";
+       permissions = "0400";
+     };
+     deployments.keys.hass-mqtt-password = {
+       text = builtins.getEnv "DEPLOY_HASS_MQTT_PASSWORD";
+       user = "hass";
+       permissions = "0400";
+     };
+     deployments.keys.kodi-api-password = {
+       text = builtins.getEnv "DEPLOY_HASS_KODI_PASSWORD";
+       user = "hass";
+       permissions = "0400";
+     };
   };
 }
