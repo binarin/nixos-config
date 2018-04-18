f79b854dfef17d5d8c391d87a74a89eed6fc9014
Alexey Lebedeff <binarin@binarin.ru>
2018-04-18 21:33:25 +0200

    Fix typo

--- nixops/personal.nix
+++ nixops/personal.nix
@@ -9,17 +9,17 @@
      imports = [
        ../configuration.nix-amon
      ];
-     deployments.keys.hass-http-password = {
+     deployment.keys.hass-http-password = {
        text = builtins.getEnv "DEPLOY_HASS_HTTP_PASSWORD";
        user = "hass";
        permissions = "0400";
      };
-     deployments.keys.hass-mqtt-password = {
+     deployment.keys.hass-mqtt-password = {
        text = builtins.getEnv "DEPLOY_HASS_MQTT_PASSWORD";
        user = "hass";
        permissions = "0400";
      };
-     deployments.keys.kodi-api-password = {
+     deployment.keys.kodi-api-password = {
        text = builtins.getEnv "DEPLOY_HASS_KODI_PASSWORD";
        user = "hass";
        permissions = "0400";
