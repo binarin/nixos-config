316a2fdf4b479742346d20869d9674f2263f596f
Alexey Lebedeff <binarin@binarin.ru>
2018-07-30 09:50:00 +0200

    Use ZFS on amon

--- partitions/unencrypted-grub-with-separate-boot-4-hdd.nix
+++ partitions/unencrypted-grub-with-separate-boot-4-hdd.nix
@@ -1,23 +1,8 @@
 {
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.devices = [ "/dev/sda" "/dev/sdb" "/dev/sdc" "/dev/sdd" ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos-root";
-      fsType = "ext4";
-      options = ["async"];
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-label/nixos-boot";
-      fsType = "ext4";
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-label/swap-0"; }
-      { device = "/dev/disk/by-label/swap-1"; }
-      { device = "/dev/disk/by-label/swap-2"; }
-      { device = "/dev/disk/by-label/swap-3"; }
-    ];
+  # swapDevices =
+  #   [ { device = "/dev/disk/by-label/swap-0"; }
+  #     { device = "/dev/disk/by-label/swap-1"; }
+  #     { device = "/dev/disk/by-label/swap-2"; }
+  #     { device = "/dev/disk/by-label/swap-3"; }
+  #   ];
 }
