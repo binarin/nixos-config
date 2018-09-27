3345a20d2085756cbd5dd73cf871e3c962bcfcb7
Alexey Lebedeff <binarin@binarin.ru>
2018-09-27 11:19:17 +0200

    More reproducibility

---
+++ modules/force-my-version.nix
@@ -0,0 +1,15 @@
+# Get version strings from my config repository and enforce them everywhere.
+# For several reasons
+# - I use several different nixpkgs checkout, so only 'nixos-config' commit id truly represents the system source
+# - commitIdFromGitRepo doesn't work with git submodules
+# - enforce truly repeatable builds of system by having just a checkout of my 'nixos-config', otherwise small changes creep into files like /etc/os-release
+
+{pkgs, lib, ...}:
+
+let
+  gitRepo      = "${toString pkgs.path}/../.git";
+  gitCommitId  = lib.substring 0 7 (lib.commitIdFromGitRepo gitRepo);
+in {
+  system.nixos.versionSuffix = "-binarin-${gitCommitId}";
+  system.nixos.label = "-binarin-${gitCommitId}";
+}
