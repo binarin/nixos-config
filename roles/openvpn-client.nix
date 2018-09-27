0103cd1165540df796391670f64488fa3f200b59
Alexey Lebedeff <binarin@binarin.ru>
2018-09-27 10:21:30 +0200

    Remove old vpns
    They were conditional on file existence, causing slightly different
    systems to be built

--- roles/openvpn-client.nix
+++ roles/openvpn-client.nix
@@ -4,12 +4,6 @@
   services.openvpn = {
     servers = {
       tcp-to-naberius = import ../vpn/tcp-to-naberius.nix { inherit config; inherit pkgs; };
-      udp-to-naberius = import ../vpn/udp-to-naberius.nix { inherit config; inherit pkgs; };
-      udp-to-amon = import ../vpn/udp-to-amon.nix { inherit config; inherit pkgs; };
-      udp-to-mirantis = import ../vpn/udp-to-mirantis.nix { inherit config; inherit pkgs; };
-      udp-to-airvpn = pkgs.lib.mkIf (config.networking.hostName == "ishamael")
-                                      (import ../vpn/udp-to-airvpn.nix { inherit config; });
-      udp-to-subscriptions = import ../vpn/udp-to-subscriptions-aws.nix { inherit config; inherit pkgs; };
     };
   };
 }
