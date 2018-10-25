6e5be623a5def216536703904960b2509ff77d81
Alexey Lebedeff <binarin@binarin.ru>
2018-10-25 10:42:35 +0200

    Switch to upstream taffybar nix-ery

--- overlay/05-taffybar-ghc822.nix
+++ overlay/05-taffybar-ghc822.nix
@@ -1,15 +1,5 @@
-# because of https://github.com/taffybar/taffybar/issues/358
 self: super:
 
-let
-  haskellOverrides = self: super: {
-    haskell-gi-overloading = super.haskell-gi-overloading_0_0;
-  };
-  haskellPackages = super.haskell.packages.ghc822.override {
-    overrides = haskellOverrides;
-  };
-in {
-  taffybar = super.taffybar.override {
-    inherit (haskellPackages) ghcWithPackages;
-  };
+{
+  taffybar = import ../taffybar/default.nix {};
 }
