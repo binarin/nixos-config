5f26ea5a56355b340515d88b9c0c38f9bf2fca10
Alexey Lebedeff <binarin@binarin.ru>
2018-12-03 16:59:17 +0100

    Still trying to override

--- overlay/07-nvidia-legacy.nix
+++ overlay/07-nvidia-legacy.nix
@@ -1,6 +1,6 @@
 # Before https://github.com/NixOS/nixpkgs/pull/51457 is merged
 self: super: {
-  linuxPackages = super.linuxPackages.extend(pSelf: pSuper: {
+  linuxPackages_4_14 = super.linuxPackages_4_14.extend(pSelf: pSuper: {
       legacy_340 = pSuper.legacy_340.overrideAttrs(oldAttrs: {
         patches = [ ../nixpkgs/pkgs/os-specific/linux/nvidia-x11/vm_operations_struct-fault.patch ];
       });
