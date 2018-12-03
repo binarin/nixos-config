c4da5671dc59b5729a6cca323940fa376d65336d
Alexey Lebedeff <binarin@binarin.ru>
2018-12-03 17:06:53 +0100

    I said 'Override!'

--- hardware/aspire-revo.nix
+++ hardware/aspire-revo.nix
@@ -1,7 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-  boot.kernelPackages = pkgs.linuxPackages_4_14;
+  boot.kernelPackages = pkgs.linuxPackages_4_14.extend(pSelf: pSuper: {
+    legacy_340 = pSuper.legacy_340.overrideAttrs(oldAttrs: {
+      patches = [ ../nixpkgs/pkgs/os-specific/linux/nvidia-x11/vm_operations_struct-fault.patch ];
+    });
+  });
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
   # boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
