42d63118ea995fb6770023d4e38e3e7249acf992
Alexey Lebedeff <binarin@binarin.ru>
2018-12-03 17:15:07 +0100

    Try it again

--- hardware/aspire-revo.nix
+++ hardware/aspire-revo.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
+  boot.kernelPackages = pkgs.linuxPackages_4_14.extend(pSelf: pSuper: {
+    nvidia_x11_legacy340 = pSuper.nvidia_x11_legacy340.overrideAttrs(oldAttrs: {
+      patches = [ ../nixpkgs/pkgs/os-specific/linux/nvidia-x11/vm_operations_struct-fault.patch ];
+    });
+  });
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
   # boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
