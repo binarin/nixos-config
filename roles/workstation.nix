767591e24db341b4d70b7064109b95e6ed2c9433
Alexey Lebedeff <binarin@binarin.ru>
2019-10-20 12:04:10 +0200

    Enable power management on barbatos

--- roles/workstation.nix
+++ roles/workstation.nix
@@ -657,6 +657,8 @@ EndSection
   # TLP brings you the benefits of advanced power management for Linux without the need to understand every technical detail.
   services.tlp.enable = true;
 
+  powerManagement.enable = if config.networking.hostName == "barbatos" then true else false;
+
   services.logind = {
     lidSwitch = "suspend";
     lidSwitchExternalPower = "ignore";
