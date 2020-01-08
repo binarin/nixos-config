c3ae82f5baf1d32028d8d792543ac3db3a579589
Alexey Lebedeff <binarin@binarin.ru>
2020-01-08 08:19:22 +0100

    Remove unnecessary parts from amon

--- nixops/personal.nix
+++ nixops/personal.nix
@@ -41,23 +41,5 @@
     imports = [
       ../configuration.nix-amon
     ];
-    deployment.keys = builtins.listToAttrs (
-      map (k: {
-        name = k;
-        value = {
-          text = builtins.getEnv "DEPLOY_${k}";
-          user = "hass";
-          permissions = "0400";
-        };
-      }) [
-        "hass_http_password"
-        "hass_mqtt_password"
-        "hass_kodi_password"
-        "hass_netatmo_api_key"
-        "hass_netatmo_secret_key"
-        "hass_netatmo_username"
-        "hass_netatmo_password"
-      ]
-    );
   };
 }
