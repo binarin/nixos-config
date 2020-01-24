d72940a420faf2ffabb3b4fd53492718d256177f
Alexey Lebedeff <binarin@binarin.ru>
2020-01-24 16:21:01 +0100

    Remove commented out emacs overrides

--- profile/emacs.nix
+++ profile/emacs.nix
@@ -2,35 +2,6 @@
 
 let
   overrides = self: self: rec {
-    # inherit (self.melpaPackages) indium haskell-mode elisp-refs vue-mode lsp-ui lsp-mode;
-
-    # lsp-vue = self.lsp-vue.overrideAttrs (oldAttrs: { # my fix for <script> linting
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "emacs-lsp";
-    #     repo = "lsp-vue";
-    #     rev = "5bf04a2ab07b426854e620416dcded2aff93d419";
-    #     sha256 = "1s8bbrp2gvhjqzmw24sq58i1y3fzy93w4896rlb8ajqzjdl9j6n4";
-    #   };
-    # });
-
-    # projectile = self.projectile.overrideAttrs (oldAttrs: { # improved alien indexing (`turbo-alien`)
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "bbatsov";
-    #     repo = "projectile";
-    #     rev = "f5c608d8945ff33e0289dad3a28c69878c2d2995";
-    #     sha256 = "06srrzm4hl6khmjfvghgvgq3812fx5fxa5wrvqvcfhwcaxs7a4i5";
-    #   };
-    # });
-
-    # intero = self.intero.overrideAttrs (oldAttrs: {
-    #   src = pkgs.fetchFromGitHub {
-    #     owner = "commercialhaskell";
-    #     repo = "intero";
-    #     rev = "8da81244783fbf03afb49660423c875f2e874fba";
-    #     sha256 = "17vibxapzp4wf0dfc56x98wsf3wy98ghj5h10nyf7xcfwy6k0rja";
-    #   };
-    # });
-
   };
   customEmacsPackages = pkgs.emacsPackagesNg.overrideScope' overrides;
   emacs-with-packages = customEmacsPackages.emacsWithPackages (p: with p; [
