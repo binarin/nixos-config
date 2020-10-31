c95eb5dbf15ca26a2b36e3a8968f7f28204efd7b
Alexey Lebedeff <binarin@binarin.ru>
2020-10-31 10:46:08 +0100

    Continue python experiments

--- python-with-emacs/python_with_emacs/__init__.py
+++ python-with-emacs/python_with_emacs/__init__.py
@@ -1,1 +1,16 @@
+import colorsys
+import collections
+
+colorsys.ONE_SIXTH
+
 __version__ = '0.1.0'
+
+collections.defaultdict(lambda: {})
+
+
+class A:
+    def __init__(self, abcd: str):
+        pass
+
+
+A(abcd=83)
