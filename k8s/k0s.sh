e93ada28117d938ecc5e4876c923be53b43753fc
Alexey Lebedeff <binarin@binarin.info>
2021-10-11 10:30:52 +0200

    k8s experiments

---
+++ k8s/k0s.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+k0sctl apply --config k0sctl.yaml
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
+# kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
+# kubectl apply -f metallb-configmap.yaml
