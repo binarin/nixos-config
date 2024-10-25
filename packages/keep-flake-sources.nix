04200114d1c291f4a86ed09fc2cd1cda35fe82b6
Alexey Lebedeff <binarin@binarin.info>
2024-10-25 19:55:27 +0200

    Protect system input flakes from garbage collection

---
+++ packages/keep-flake-sources.nix
@@ -0,0 +1,8 @@
+({ lib, inputs, ... }: {
+  environment.etc = builtins.listToAttrs (builtins.map (input:
+  lib.attrsets.nameValuePair "sources/${input}" {
+    enable = true;
+    source = inputs.${input};
+    mode = "symlink";
+  }) (builtins.attrNames inputs));
+})
