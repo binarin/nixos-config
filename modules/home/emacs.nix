a5a32766b634bd5eb621e5df75b326c49aa7dbb8
Alexey Lebedev <johndoe@furfur.com>
2024-10-27 20:55:16 +0000

    Clean-up emacs-config.org for use-package magic in emacs-overlay

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -3,7 +3,7 @@
     (pkgs.emacsWithPackagesFromUsePackage {
       alwaysTangle = true;
       config = pkgs.runCommand "cleanup-unicode-from-emacs-config.org" { } ''
-        ${lib.getExe' pkgs.coreutils "tr"} -c -d '\000-\199'< ${flake.inputs.self + "/users/emacs-config.org"} > $out
+        ${lib.getExe' pkgs.coreutils "tr"} -c -s '\000-\177' x < ${flake.inputs.self + "/users/emacs-config.org"} > $out
       '';
     }
     )
