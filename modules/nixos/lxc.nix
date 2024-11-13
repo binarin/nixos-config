aad0366bb31b0bd29aa047cdb62cab9532ea29b8
Alexey Lebedeff <binarin@binarin.info>
2024-11-13 16:59:28 +0100

    Disable proxmoxLXC module by default
    It's an optional module that needs to be explicitly loaded, and
    auto-enabled itself.

--- modules/nixos/lxc.nix
+++ modules/nixos/lxc.nix
@@ -9,23 +9,29 @@ in
     inputs.nixos-generators.nixosModules.all-formats
   ];
 
-  config = lib.mkIf (config.hostConfig.feature.lxc) {
-    proxmoxLXC.enable = true;
+  config = lib.mkMerge [
+    (lib.mkIf (config.hostConfig.feature.lxc) {
+      proxmoxLXC.enable = true;
 
-    proxmoxLXC.manageNetwork = true;
-    networking.useNetworkd = true;
-    networking.useHostResolvConf = false;
-    networking.useDHCP = false;
+      proxmoxLXC.manageNetwork = true;
+      networking.useNetworkd = true;
+      networking.useHostResolvConf = false;
+      networking.useDHCP = false;
 
-    systemd.network.networks."40-lxc" = {
-      matchConfig.Name = "eth0";
-      dns = config.inventory.networks.home.dns;
-      address = [ config.hostConfig.ipAllocation.home.primary.addressWithPrefix ];
-      routes = [
-        { routeConfig.Gateway = config.inventory.networks.home.gateway; }
-      ];
-    };
+      systemd.network.networks."40-lxc" = {
+        matchConfig.Name = "eth0";
+        dns = config.inventory.networks.home.dns;
+        address = [ config.hostConfig.ipAllocation.home.primary.addressWithPrefix ];
+        routes = [
+          { routeConfig.Gateway = config.inventory.networks.home.gateway; }
+        ];
+      };
 
-    services.getty.autologinUser = "root";
-  };
+      services.getty.autologinUser = "root";
+    })
+    (lib.mkIf (config.hostConfig.feature.lxc) {
+      proxmoxLXC.enable = lib.mkDefault false;
+    })
+  ];
+
 }
