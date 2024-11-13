68c555b92c7f144451d0e897e6f156aa2cd76cc5
Alexey Lebedeff <binarin@binarin.info>
2024-11-13 16:56:23 +0100

    Add missing package brightnessctl-all

---
+++ packages/brightnessctl-all.nix
@@ -0,0 +1,15 @@
+{writeShellApplication, brightnessctl, coreutils, ...}:
+writeShellApplication {
+  name = "brightnessctl-all";
+  runtimeInputs = [ brightnessctl coreutils ];
+  text = ''
+    mapfile -t all < <(brightnessctl -l -c backlight -m | cut -d , -f1)
+    exit_code=0
+    for dev in "''${all[@]}" ; do
+      if ! brightnessctl -d "$dev" "$@"; then
+        exit_code=$?
+      fi
+    done
+    exit $exit_code
+  '';
+}
