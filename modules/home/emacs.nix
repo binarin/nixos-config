a1f80d8d9d26074807a9215e7203e918b8d8d8dd
Alexey Lebedeff <binarin@binarin.info>
2024-11-13 16:58:47 +0100

    Add treesitter rust (for emacs rust mode)

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -7,7 +7,10 @@ in
     home.file.".emacs.d/init.el".source = cfg.compiledConfig + "/init.el";
     home.file.".emacs.d/init.elc".source = cfg.compiledConfig + "/init.elc";
     fonts.nerdfonts = [ "IosevkaTerm" ];
-    home.packages = [ pkgs.emacs-all-the-icons-fonts ];
+    home.packages = [
+      pkgs.emacs-all-the-icons-fonts
+      (pkgs.tree-sitter.withPlugins (p: [ p.tree-sitter-rust ]))
+    ];
     home.sessionVariables.EDITOR = "emacsclient -a 'emacs -nw' -nw";
   };
 }
