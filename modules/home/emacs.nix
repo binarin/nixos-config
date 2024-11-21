c4d8176dd4785986ffc8840f4fe813ed7fa8cfc5
Alexey Lebedeff <binarin@binarin.info>
2024-11-21 20:04:47 +0100

    Restore org-protocol desktop file
    The one from emacs itself is not working with `xdg-open` in `generic` DE

--- modules/home/emacs.nix
+++ modules/home/emacs.nix
@@ -6,7 +6,14 @@ in
   config = {
     home.file.".emacs.d/init.el".source = cfg.compiledConfig + "/init.el";
     home.file.".emacs.d/init.elc".source = cfg.compiledConfig + "/init.elc";
+
+    home.file.".local/share/applications/org-protocol.desktop".source = pkgs.flakeFile "org-protocol.desktop";
+    xdg.mimeAppsdefaultApplications = lib.mkIf pkgs.stdenv.isLinux {
+      "x-scheme-handler/org-protocol" = "org-protocol.desktop";
+    };
+
     fonts.nerdfonts = [ "IosevkaTerm" ];
+
     home.packages = [
       pkgs.emacs-all-the-icons-fonts
       (pkgs.tree-sitter.withPlugins (p: [ p.tree-sitter-rust ]))
