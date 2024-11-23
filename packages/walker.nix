57a29b6a94bf664c867a1716b5999cb89995111b
Alexey Lebedeff <binarin@binarin.info>
2024-11-23 19:32:59 +0100

    Add latest walker package

---
+++ packages/walker.nix
@@ -0,0 +1,13 @@
+{pkgs, fetchFromGitHub, ...}:
+pkgs.bleeding.walker.overrideAttrs (finalAttrs: prevAttrs: {
+  version = "0.9.7";
+
+  src = fetchFromGitHub {
+    owner = "abenz1267";
+    repo = "walker";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-LxN1J38OH2kSDRmQHCB++UjL92JKcbuZnJOTkFImHxA=";
+  };
+
+  vendorHash = "sha256-nc/WKBhUxhs1aNUg/GM7vhrKd7FrUdl2uKp7MX2VCdE=";
+})
