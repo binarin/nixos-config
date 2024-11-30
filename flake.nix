1b8d73074bfeeb02e788fbe16460a4ab793b08eb
Alexey Lebedeff <binarin@binarin.info>
2024-11-30 15:35:50 +0100

    Cleanup waybar experiments

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
 
     nixpkgs-unstable.url = "github:nixos/nixpkgs";
 
+    # home-manager.url = "path:/home/binarin/personal-workspace/home-manager";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
