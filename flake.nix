5019b990cbd048aed60871a66ac9f2fcf8109830
Alexey Lebedeff <binarin@binarin.info>
2024-12-02 16:55:13 +0100

    Bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    # nixpkgs.url = "path:/home/binarin/personal-workspace/nixpkgs";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     nixpkgs-unstable.url = "github:nixos/nixpkgs";
