678a55ac0cf8099a04530274e6bfa6e3851d49e9
Alexey Lebedeff <binarin@binarin.info>
2024-12-03 12:25:31 +0100

    ishamael: make it buildable

--- configurations/nixos/ishamael/configuration.nix
+++ configurations/nixos/ishamael/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, modulesPath, ... }:
 
 {
   imports =
@@ -55,6 +55,7 @@
   swapDevices = [ ];
 
   # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 }
