0fd04f984ec5f727e095557fca48797c3806eaaf
Alexey Lebedeff <binarin@binarin.info>
2025-02-16 13:50:27 +0100

    wezterm: fix .desktop file, it wasn't re-attaching with the default one

--- modules/home/wezterm.nix
+++ modules/home/wezterm.nix
@@ -9,5 +9,19 @@ in {
     };
     # xdg.configFile."wezterm/wezterm.lua".source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink "/home/binarin/personal-workspace/nixos-config/files/wezterm.lua");
     xdg.configFile."wezterm/wezterm.lua".source = lib.mkForce (config.lib.self.file "wezterm.lua");
+
+    xdg.dataFile."applications/org.wezfurlong.wezterm.desktop".source = pkgs.writeText "org.wezfurlong.wezterm.desktop" ''
+      [Desktop Entry]
+      Name=WezTerm
+      Comment=Wez's Terminal Emulator
+      Keywords=shell;prompt;command;commandline;cmd;
+      Icon=org.wezfurlong.wezterm
+      StartupWMClass=org.wezfurlong.wezterm
+      TryExec=wezterm
+      Exec=wezterm
+      Type=Application
+      Categories=System;TerminalEmulator;Utility;
+      Terminal=false
+    '';
   };
 }
