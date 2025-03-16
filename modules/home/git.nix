17f66e0bac70af98e0cd379bfd2d4676c47cf05d
Alexey Lebedeff <binarin@binarin.info>
2025-03-16 20:58:22 +0100

    Experimenting with recommended git options

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -20,6 +20,17 @@ in
       package = pkgs.gitAndTools.gitFull;
       delta.enable = true;
       extraConfig = {
+        column.ui = "auto";
+        branch.sort = "-committerdate";
+        tag.sort = "version:refname";
+        diff.algorithm = "histogram";
+        diff.mnemonicPrefix = true;
+
+        fetch.prune = true;
+        fetch.pruneTags = true;
+        fetch.all = true;
+        help.autocorrect = "prompt";
+
         core = {
           autocrlf = false;
         };
@@ -33,9 +44,12 @@ in
             pushInsteadOf = "fj:";
           };
         };
-        commit = {
-          template = "${config.lib.self.file "git-commit-template.txt"}";
-        };
+        commit.verbose = true;
+        commit.template = "${config.lib.self.file "git-commit-template.txt"}";
+        rerere.enabled = true;
+        rerere.autoupdate = true;
+        merge.conflictstyle = "zdiff3";
+
         "delta \"decorations\"" = {
           commit-decoration-style = "bold yellow box ul";
           file-style = "bold yellow ul";
