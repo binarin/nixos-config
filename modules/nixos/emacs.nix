9c260c563a64d12394b0aa7f4ed79311b18ff08d
Alexey Lebedeff <binarin@binarin.info>
2025-06-02 16:51:54 +0200

    Add emacs renaming workarounds to NixOS module

--- modules/nixos/emacs.nix
+++ modules/nixos/emacs.nix
@@ -14,6 +14,8 @@ in
     nixpkgs.overlays = [
       flake.inputs.emacs-overlay.overlays.default
       (final: prev: {
+        emacs-pgtk-saved = prev.emacs-pgtk;
+        emacs-saved = prev.emacs;
         tree-sitter = final.bleeding.tree-sitter;
       })
     ];
