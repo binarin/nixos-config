9179fd879ae07311a6cbce3b3237c5eeef228833
Alexey Lebedeff <binarin@binarin.info>
2025-06-20 09:07:57 +0200

    Revert "tandoor: only proxy /api/* to tandoor docker"
    Is this actually a permissions issue?

--- configurations/nixos/media/tandoor.nix
+++ configurations/nixos/media/tandoor.nix
@@ -28,7 +28,7 @@
         root /var/lib/tandoor/staticfiles
       }
 
-      reverse_proxy /api/* http://127.0.0.1:8081
+      reverse_proxy http://127.0.0.1:8081
       import letsencrypt
     '';
 
