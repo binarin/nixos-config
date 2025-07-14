7e4ed84c24cffb8242bdaa6a296270f31e9301e8
Alexey Lebedeff <binarin@binarin.info>
2025-07-14 13:13:14 +0200

    Enable GUI stylix targets in home-manager only when gui is enabled
    Otherwise it tries to pull in (un-available) dconf service from DBUS
    during the activation.

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -9,30 +9,38 @@
   imports = [
     flake.inputs.stylix.homeModules.stylix
   ];
-  config = {
-    # XXX Another copy in nixos/stylix.nix
-    stylix.cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Amber";
-      size = 16;
-    };
+  config = lib.mkMerge [
+    {
+      # XXX Another copy in nixos/stylix.nix
+      stylix.cursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Amber";
+        size = 16;
+      };
 
-    stylix.targets =
-      lib.genAttrs
-        [
-          "bat"
-          "tmux"
-          "btop"
-          "firefox"
-          "fzf"
-          "gtk"
-          "swaync"
-          "vscode"
-          "fuzzel"
-          "zellij"
-        ]
-        (nm: {
-          enable = true;
-        });
-  };
-}
+      stylix.targets =
+        lib.genAttrs
+          [
+            "bat"
+            "tmux"
+            "btop"
+            "fzf"
+            "zellij"
+          ]
+          (nm: {
+            enable = true;
+          });
+    }
+    (lib.mkIf config.hostConfig.feature.gui {
+      stylix.targets = lib.genAttrs [
+        "swaync"
+        "vscode"
+        "fuzzel"
+        "firefox"
+        "gtk"
+      ] (nm: {
+        enable = true;
+      });
+    })
+  ];
+  }
