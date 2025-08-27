a6c62c4820fab7a8c35887d049e2c0b2ce220f64
Alexey Lebedeff <binarin@binarin.info>
2025-08-27 11:52:41 +0200

    Enable eternal-terminal service on NixOS
    It's automatically enabled on NixOS machines that are intended for
    interactive CLI usage.

---
+++ modules/nixos/eternal-terminal.nix
@@ -0,0 +1,9 @@
+{flake, pkgs, config, lib, ...}:
+{
+  config = lib.mkIf config.hostConfig.feature.interactive-cli {
+    services.eternal-terminal = {
+      enable = true;
+    };
+    networking.firewall.allowedTCPPorts = [ config.services.eternal-terminal.port ];
+  };
+}
