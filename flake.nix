11562b21fa739c4296f9e984dc3ea7158a21b24f
Alexey Lebedeff <binarin@binarin.info>
2025-10-26 17:54:32 +0100

    kmonad: revert bump, it was actually a typo of 'ref'/'rev'

--- flake.nix
+++ flake.nix
@@ -43,8 +43,7 @@
 
     kmonad = {
       url = "https://github.com/kmonad/kmonad";
-      # ref = "refs/tags/0.4.4";
-      ref = "7f8a0b38d4777fbacf3629b6ff8805e8eeec2f19";
+      rev = "a38004ff4df4beda648ec5b1374173d3192d61ae";
       type = "git";
       submodules = true;
       dir = "nix";
