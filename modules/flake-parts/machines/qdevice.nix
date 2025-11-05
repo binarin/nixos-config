c9e1a673baed331ed7e2f4caf9ebf99184dcb55f
Alexey Lebedeff <binarin@binarin.info>
2025-11-05 19:03:18 +0100

    qdevice: setting up virtualisation env

--- modules/flake-parts/machines/qdevice.nix
+++ modules/flake-parts/machines/qdevice.nix
@@ -6,6 +6,30 @@ nix run github:nix-community/nixos-anywhere -- \
  --target-host root@192.168.2.16 \
  --disk-encryption-keys /tmp/password-without-newline /tmp/password-without-newline
 '';
+
+debian = ''
+NAME=qdevice
+DIST_URL="http://ftp.debian.org/debian/dists/trixie/main/installer-amd64/"
+LINUX_VARIANT="debian"
+
+virt-install \
+--connect=qemu:///system \
+--name=${NAME} \
+--ram=1024 \
+--vcpus=2 \
+--disk size=16,path=/var/lib/libvirt/images/${NAME}.img,bus=virtio,cache=none \
+--initrd-inject=preseed.cfg \
+--initrd-inject=postinst.sh \
+--initrd-inject=postinst.tar.gz \
+--location ${DIST_URL} \
+--os-variant ${LINUX_VARIANT} \
+--virt-type=kvm \
+--controller usb,model=none \
+--graphics none \
+--noautoconsole \
+--network bridge=br0,mac=${MAC},model=virtio \
+--extra-args="auto=true hostname="${1}" domain="${DOMAIN}" console=tty0 console=ttyS0,115200n8 serial"
+'';
 in {
   flake.deploy.nodes.qdevice = {
     hostname = "192.168.2.16";
@@ -56,15 +80,37 @@ in {
       users.users.root.openssh.authorizedKeys.keys = config.lib.publicKeys.secureWithTag "presence";
 
       networking.useDHCP = false;
-      systemd.network.enable = true;
-
       networking.hostName = inventoryHostname;
 
-      systemd.network.networks."40-enp2s0" = {
-        matchConfig.Name = "enp2s0";
-        dns = [ "192.168.2.1" ];
-        address = [ "192.168.2.16/24" ];
-        routes = [ { Gateway = "192.168.2.1"; } ];
+      systemd.network = {
+        enable = true;
+        netdevs = {
+          "20-br0" = {
+            netdevConfig = {
+              Kind = "bridge";
+              Name = "br0";
+            };
+          };
+        };
+
+        networks = {
+          "30-enp2s0" = {
+            matchConfig.Name = "enp2s0";
+            networkConfig.Bridge = "br0";
+            linkConfig.RequiredForOnline = "enslaved";
+          };
+
+          "40-br0" = {
+            matchConfig.Name = "br0";
+            dns = [ "192.168.2.1" ];
+            address = [ "192.168.2.16/24" ];
+            routes = [ { Gateway = "192.168.2.1"; } ];
+            bridgeConfig = { };
+            linkConfig = {
+              RequiredForOnline = "routable";
+            };
+          };
+        };
       };
 
       boot.initrd.systemd.network.networks."40-enp2s0" = {
@@ -78,6 +124,20 @@ in {
 
       networking.firewall.allowedTCPPorts = [
         7654
+      ];
+
+      virtualisation.libvirtd = {
+        enable = true;
+        qemu.ovmf.enable = true;
+        qemu.ovmf.packages = [ pkgs.OVMFFull.fd ];
+        qemu.runAsRoot = false;
+        qemu.swtpm.enable = true;
+        onBoot = "ignore"; # autostarded guests are started anyway
+        onShutdown = "shutdown";
+      };
+
+      environment.systemPackages = with pkgs; [
+        virt-manager
       ];
 
       services.tang = {
