eb21ca472b12e12f6a4226cdf39f6d172ca2fc37
Alexey Lebedeff <binarin@binarin.info>
2025-11-05 09:26:54 +0100

    Clean-up ssh keys

--- inventory/public-keys.nix
+++ inventory/public-keys.nix
@@ -1,24 +1,13 @@
 {
   ssh_keys = {
-    pixel-8-pro-bio = {
-      description = "Pixel 8 Pro (GrapheneOS) biometrics key in Termius";
-      public_key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBA+FKpMFeDrObDAqF9Tc0gTDw81J0J2uoDccGzcxzHyWC+dBbmToaKAGNFmAUwFX5U8sazTNTKT2EFEwxd8mHIU=";
-      secure = true;
-      tags = [ "default" ];
-    };
-
-    galaxy-tab = {
-      description = "Galaxy Tab biometrics key in Termius";
-      public_key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPMpogmbCYL1eZcImgz+QDH5xW5BoTDUP4/daxglmaBul2tp9SFnMT7f73Z9rVF5XSsavVm4j8mLNV6t3I2xCKI=";
-      secure = true;
-      tags = [ "default" ];
-    };
-
     trezor-root-pve = {
       description = "Trezor ssh key with root@pve identity";
       public_key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAAHsiCVekjyoPDPOGPJFJ5z9FQUfSmThnGWHABNfjz0APf/qcdHHy+Yj2bG0ICcDbBrE10MJiQjrnYRlKUGvAE=";
       secure = true;
-      tags = [ "default" ];
+      tags = [
+        "default"
+        "presence"
+      ];
     };
 
     "raum.lynx-lizard.ts.net-pkcs11-root" = {
@@ -53,38 +42,44 @@
     };
 
     "yubi-usb-c-5-nfc" = {
-      description = "resident SSH key on Yubikey 5 NFC (usb-c)";
+      description = "resident SSH key on Yubikey 5 NFC (usb-c, keychain)";
       public_key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDaRvW2TCKgLVQuHAv5FyNqQuFidJ+5WhIaV9j6nGU3HAAAABHNzaDo= usb-c yubi - residen ssh fido2";
       secure = true;
-      tags = [ "default" "user-ca" ];
-    };
-
-    "yubi-usb-a-security-key-nfc" = {
-      description = "resident SSH key on Yubikey Security Key (usb-a)";
-      public_key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBecanJ4q2FCAu5iw1Ah+19Ki5k9bncJ2ROXQQn/Bv8rAAAABHNzaDo= usb-a yubikey security key(fido only)  - resident ssh fido2";
-      secure = true;
-      tags = [ "default" "user-ca" ];
+      tags = [
+        "default"
+        "user-ca"
+        "presence"
+      ];
     };
 
     "yubi-usb-a-5-nfc-fixed" = {
       description = "resident SSH key on Yubikey 5 NFC (usb-a, fixed)";
       public_key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEriEHU1OODgpI5oWyeH0zjyUA8N5MsgUiiYIVNXMdQrAAAABHNzaDo= usb-a yubikey 5 nfc - fixed  - resident ssh fido2";
       secure = true;
-      tags = [ "default" "user-ca" ];
+      tags = [
+        "default"
+        "user-ca"
+        "presence"
+      ];
     };
 
     "yubi-usb-a-5-nfc-keychain" = {
       description = "resident SSH key on Yubikey 5 NFC (usb-a, keychain)";
       public_key = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINC200GwDhLpi27iCDBppww5QPuRtXACjXsrtJ5hnW0LAAAABHNzaDo= usb-a yubikey 5 nfc - keychain  - resident ssh fido2";
       secure = true;
-      tags = [ "default" "user-ca" ];
+      tags = [
+        "default"
+        "user-ca"
+      ];
     };
 
     "trezor-trusted-user-ca" = {
       description = "trezor-agent with 'trusted-user-ca' identity";
       public_key = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKx4e7PnTrPabkbTwYVsCum+j/Fxgs46NR2ltvgqmUnsJyxVm42rhoqNBp9+1D2Xt0WxikWUBXpe5liSPZzFleo=";
       secure = true;
-      tags = [ "user-ca" ];
+      tags = [
+        "user-ca"
+      ];
     };
 
     brother-scanner = {
