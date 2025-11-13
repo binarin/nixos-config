8827d3f1ff81fb827dd8471832ac698c8c0a6cd1
Alexey Lebedeff <binarin@binarin.info>
2025-11-13 09:36:17 +0100

    nix fmt

--- modules/binarin/syncthing.nix
+++ modules/binarin/syncthing.nix
@@ -1,31 +1,34 @@
-{ inputs, ...}: {
-  flake.homeModules.syncthing = {config, ...}: {
-    key = "nixos-config.modules.home.syncthing";
+{ inputs, ... }:
+{
+  flake.homeModules.syncthing =
+    { config, ... }:
+    {
+      key = "nixos-config.modules.home.syncthing";
 
-    disabledModules = [
-      "${inputs.home-manager}/modules/services/syncthing.nix"
-    ];
-    imports = [
-      "${inputs.home-manager-master}/modules/services/syncthing.nix"
-    ];
+      disabledModules = [
+        "${inputs.home-manager}/modules/services/syncthing.nix"
+      ];
+      imports = [
+        "${inputs.home-manager-master}/modules/services/syncthing.nix"
+      ];
 
-    sops.secrets."syncthing-ui-password" = {};
+      sops.secrets."syncthing-ui-password" = { };
 
-    services.syncthing = {
-      enable = true;
-      tray.enable = true;
-      passwordFile = config.sops.secrets."syncthing-ui-password".path;
-      settings = {
-        devices = {
-          pixel8.id = "ROLQIQ6-OEAMVJA-KRM4IDA-KOGUXR6-RZW3LRR-C6VZSAF-LZL5ZEV-PWAQTQL";
-        };
-        folders = {
-          "/home/binarin/Music" = {
-            id = "demandred-music";
-            devices = [ "pixel8" ];
+      services.syncthing = {
+        enable = true;
+        tray.enable = true;
+        passwordFile = config.sops.secrets."syncthing-ui-password".path;
+        settings = {
+          devices = {
+            pixel8.id = "ROLQIQ6-OEAMVJA-KRM4IDA-KOGUXR6-RZW3LRR-C6VZSAF-LZL5ZEV-PWAQTQL";
+          };
+          folders = {
+            "/home/binarin/Music" = {
+              id = "demandred-music";
+              devices = [ "pixel8" ];
+            };
           };
         };
       };
     };
-  };
 }
