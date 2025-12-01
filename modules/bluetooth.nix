e8976c7b858180f6aaa50c6db8716ff2d96c2572
Alexey Lebedeff <binarin@binarin.info>
2025-12-01 16:15:18 +0100

    Fix bootstrapping of bluetooth impermanence

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -21,8 +21,16 @@
           powerOnBoot = true;
         };
 
+        systemd.tmpfiles.settings."10-bluetooth-persist" = lib.mkIf config.impermanence.enable {
+          "/persist/var/lib/bluetooth".d = {
+            user = "root";
+            group = "root";
+            mode = "0700";
+          };
+        };
+
         systemd.services.bluetooth.serviceConfig.BindPaths = lib.mkIf config.impermanence.enable [
-          "/local/var/lib/bluetooth:/var/lib/bluetooth"
+          "/persist/var/lib/bluetooth:/var/lib/bluetooth"
         ];
       };
     };
