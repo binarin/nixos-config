8c0ad48fbbc3ddae83ab57a16486d58758486d20
Alexey Lebedeff <binarin@binarin.info>
2025-12-01 09:03:31 +0100

    update caddy-with-plugins hash, after caddy version change

--- modules/machines/media.nix
+++ modules/machines/media.nix
@@ -193,7 +193,7 @@
             plugins = [
               "github.com/caddy-dns/cloudflare@v0.0.0-20251022184029-2fc25ee62f40"
             ];
-            hash = "sha256-8eyfR+0TY5GEyFmYaW/NXEK2n8iVQYATP9Ma2UzhQCQ";
+            hash = "sha256-sexPn0LzErmK8ptUICUPSSqLNLYIy7F9M3JBJfyCpJQ";
           };
           extraConfig = ''
             (letsencrypt) {
