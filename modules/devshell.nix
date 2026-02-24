a93de692e053e00bd1fc67023ffabcf0752baea1
Alexey Lebedeff <binarin@binarin.info>
2026-02-24 07:13:58 +0100

    fixup! #57: Fix ncf command not found in iso-wifi CI

--- modules/devshell.nix
+++ modules/devshell.nix
@@ -16,6 +16,8 @@
       '';
     in
     {
+      packages.ncf = ncf;
+
       devShells.default = pkgs.mkShell {
         name = "nixos-unified-template-shell";
         meta.description = "Shell environment for modifying this Nix configuration";
