82417567ddb1479fa02201ab805e7f1c481bf8c0
Alexey Lebedeff <binarin@binarin.info>
2026-03-05 18:31:44 +0100

    use my personal nix-cache on all machines

--- modules/baseline/default.nix
+++ modules/baseline/default.nix
@@ -16,6 +16,7 @@
         self.nixosModules.monitored
 
         self.nixosModules.eternal-terminal
+        self.nixosModules.use-nix-cache
 
         self.nixosModules.binarin-baseline
 
--- modules/default.nix
+++ modules/default.nix
@@ -12,23 +12,14 @@
       imports = [
         self.nixosModules.baseline
         self.nixosModules.emacs
-        self.nixosModules.eternal-terminal
         self.nixosModules.git
         self.nixosModules.interactive-cli
-        self.nixosModules.nix
-        self.nixosModules.security
         self.nixosModules.sops
-        self.nixosModules.sshd
         self.nixosModules.tailscale
-        self.nixosModules.use-nix-cache
       ];
-
       config = {
         system.switch.enable = true;
-
         services.dbus.implementation = lib.mkDefault "broker";
-
-        time.timeZone = lib.mkDefault "Europe/Amsterdam";
       };
     };
 }
