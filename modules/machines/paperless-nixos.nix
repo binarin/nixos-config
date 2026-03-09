235756df6b6c26875aac1f8b161a30b2f439ff4d
Alexey Lebedeff <binarin@binarin.info>
2026-03-09 18:19:19 +0100

    deploy paperless-nixos

--- modules/machines/paperless-nixos.nix
+++ modules/machines/paperless-nixos.nix
@@ -253,6 +253,11 @@ in
           };
         };
 
+        sops.secrets.tailscale-auth = { };
+        services.tailscale = {
+          authKeyFile = "${config.sops.secrets.tailscale-auth.path}";
+        };
+
         # Expose Paperless as a Tailscale service
         services.tailscale.serve = {
           enable = mkDefault true;
