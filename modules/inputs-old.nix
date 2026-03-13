d17ae41aeea1c63a8a134a377baa88e4726cfab6
Alexey Lebedeff <binarin@binarin.info>
2026-03-13 11:58:39 +0100

    Enable aarch64-linux system - for rpi

--- modules/inputs-old.nix
+++ modules/inputs-old.nix
@@ -4,7 +4,10 @@
     inputs.flake-file.flakeModules.dendritic
   ];
 
-  systems = [ "x86_64-linux" ];
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
 
   flake-file.outputs = lib.mkForce ''
     inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules)
