3f4a8c24f705da4af7a6f876edcdb78acf1bed21
Alexey Lebedeff <binarin@binarin.info>
2026-03-13 11:06:18 +0100

    drop impure nix hacks
    can pick configured nixpkgs with all overlays from flake, like this:
    
    nix run --impure --expr '(builtins.getFlake "/home/binarin/personal-workspace/nixos-config").nixosConfigurations.'"$(hostname)"'.pkgs.emacs-git-pgtk.pkgs.withPackages (p: with p; [direnv magit zenburn-theme vertico orderless corfu nix-mode treesit-grammars.with-all-grammars])' . --  --init-directory=$HOME/personal-workspace/nixos-config/files/emacs/ --debug-init

--- files/nixos-configuration-nix-config-for-impure-nix-commands.nix
+++
@@ -1,8 +0,0 @@
-let
-  flake =
-    with builtins;
-    getFlake (unsafeDiscardStringContext (readFile /etc/nix/inputs/self-absolute-path));
-  host = builtins.head (builtins.split "\n" (builtins.readFile "/etc/hostname"));
-  configuration = flake.nixosConfigurations."${host}";
-in
-configuration.config.nixpkgs.config
--- modules/impure-nix-setup.nix
+++ modules/impure-nix-setup.nix
@@ -15,20 +15,6 @@
           gc-keep-derivations = true
         '';
 
-        environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
-        environment.etc."nix/inputs/self-absolute-path".text = "${self}";
-        environment.etc."nix/overlays/self-overlays.nix".source =
-          config.lib.self.file "nixos-configuration-overlays-for-impure-nix-commands.nix";
-        environment.etc."nix/nixpkgs-config.nix".source =
-          config.lib.self.file "nixos-configuration-nix-config-for-impure-nix-commands.nix";
-
-        environment.variables.NIXPKGS_CONFIG = "/etc/nix/nixpkgs-config.nix";
-
-        nix.nixPath = [
-          "nixpkgs=/etc/nix/inputs/nixpkgs"
-          "nixpkgs-overlays=/etc/nix/overlays"
-        ];
-
         nix.registry.nixpkgs.flake = inputs.nixpkgs;
         nix.registry.nixpkgs-unstable.flake = inputs.nixpkgs-unstable;
         nix.registry.flake-parts.flake = inputs.flake-parts;
