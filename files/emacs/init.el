d6ffb3c5ea70991f1180cc556faa51c3e9f40707
Alexey Lebedeff <binarin@binarin.info>
2026-03-19 22:33:34 +0100

    emacs-clean: implement format-on-save for nix (and in general)

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -181,10 +181,58 @@
 
 (add-hook 'yaml-ts-mode-hook #'b/yaml-mode-hook)
 
+(defvar b/format-buffer-with-error-buffer)
+(make-variable-buffer-local 'b/format-buffer-with-error-buffer)
+
+(cl-defun b/format-buffer-with (prog &key display-errors args)
+  (let ((stdout-file (make-temp-file "*fmt out"))
+	(stderr-file (make-temp-file "*fmt err"))
+	stderr-buffer)
+    (unwind-protect
+      (save-restriction
+	(widen)
+	(let ((exit-code
+	       (apply #'call-process-region nil nil ;; start, end
+				    prog
+				    nil ;; delete
+				    (list (list :file stdout-file) stderr-file) ;; buffer
+				    nil ;; display
+				    args)))
+	  (if (and (numberp exit-code) (eql exit-code 0))
+	      (progn
+		(insert-file-contents stdout-file nil nil nil t)
+		(when (buffer-live-p b/format-buffer-with-error-buffer)
+		  (delete-windows-on b/format-buffer-with-error-buffer)
+		  (setf b/format-buffer-with-error-buffer nil)))
+	    (progn
+	      (unless (buffer-live-p b/format-buffer-with-error-buffer)
+		(setf b/format-buffer-with-error-buffer
+		      (generate-new-buffer (format "%s errors" (buffer-file-name)) t)))
+	      (setf stderr-buffer b/format-buffer-with-error-buffer)
+	      (with-current-buffer b/format-buffer-with-error-buffer
+		(let ((inhibit-read-only t))
+		  (insert-file-contents stderr-file nil nil nil t))
+		(compilation-mode)
+		(if display-errors
+		    (display-buffer stderr-buffer)
+		  (message "'%s' failed: see '%s'" prog stderr-buffer)))))))
+      (delete-file stderr-file)
+      (delete-file stdout-file))))
+
+(defun b/format-nix (&optional display-errors)
+  (interactive "P")
+  (b/format-buffer-with
+   "nixfmt"
+   :display-errors display-errors
+   :args (list "-f" (file-name-nondirectory (buffer-file-name)))))
+
+(defun b/nix-mode-hook ()
+  (add-hook 'before-save-hook #'b/format-nix nil t))
+
 (use-package nix-mode
   :ensure t
-  :mode ("\\.nix\\'" . nix-mode))
-
+  :mode ("\\.nix\\'" . nix-mode)
+  :hook ((nix-mode . b/nix-mode-hook)))
 
 
 (use-package magit
