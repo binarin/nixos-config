e0f07662e172c3d78218b21afc92020666fdc6ae
Alexey Lebedeff <binarin@binarin.info>
2026-03-20 13:58:09 +0100

    emacs CI: debug

--- scripts/test-byte-compile.sh
+++ scripts/test-byte-compile.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-
+set -x
 nix run .#emacs-clean-nox -- \
-    -Q -L ./files/emacs/ --batch \
+    -Q \
+    -L ./files/emacs/ \
+    --batch \
     --eval '(setf byte-compile-error-on-warn t)' \
+    --eval '(message "%s" load-path)' \
     --eval '(unless (byte-compile-file "'"$1"'") (kill-emacs 1))'
