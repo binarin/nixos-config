1dd6b2aee3ed402daf7a1c924379fc6a40291831
Alexey Lebedeff <binarin@binarin.info>
2026-03-23 20:23:50 +0100

    emacs: b/ripgrep - use compilation error levels to make --headings work
    by marking file-name as info/warnings, it's possible to skip them (for
    next-error purposes) when --headings are used

--- files/emacs/lisp/b-ripgrep.el
+++ files/emacs/lisp/b-ripgrep.el
@@ -25,6 +25,8 @@ function properly")
 (defvar b/ripgrep-match-regexp (rx "\033[36m" (group (+ (not "\033"))))
   "`cyan' from b/ripgrep-base-args")
 
+(defvar b/ripgrep-use-headings t)
+
 (defvar b/ripgrep-history nil)
 
 (defvar compilation-filter-start)
@@ -46,6 +48,8 @@ function properly")
 				     'face nil 'font-lock-face grep-match-face)
 			 t t)
 	  (incf grep-num-matches-found))
+	(when b/ripgrep-use-headings
+	  (replace-string-in-region "\0" "\0\n" beg end))
 	;; clean up the rest of escapes
 	(goto-char beg)
 	(while (re-search-forward "\033\\[[0-9;]*[mK]" end 1)
@@ -66,24 +70,43 @@ function properly")
     (when mend
       (- mend beg))))
 
-(defvar b/ripgrep-error-regexp-alist
-  `((,(rx bol
+
+(defvar b/ripgrep-error-regexp-alist-alist
+  `((rg-file-line
+     ,(rx bol
 	  (? (group-n 1 (+ (not (any "\0\n"))))
 	     (group-n 3 "\0"))
 	  (group-n 2 (+ (any digit)))
 	  ":")
      1 2
      ,(cons #'b/ripgrep--col-beg #'b/ripgrep--col-end)
-     nil nil
+     2 ;; file with line number -> error
+     nil
      (3 '(face nil display ":")))
-    (,(rx bol (group-n 1 (+? not-newline)) ": binary file matches") 1 nil nil 0 1)))
+    (rg-file-only
+     ,(rx bol (group-n 1 (+ (not (any "\0\n")))) (group-n 3 "\0"))
+     1 nil nil
+     1 ;; file without line number(heading) -> warning
+     nil
+     (3 '(face nil display "")))
+    (rg-binary-file
+     ,(rx bol (group-n 1 (+? not-newline)) ": binary file matches")
+     1 nil nil
+     0 ;; binary file -> error
+     1)
+    (rg-line-only
+     ,(rx bol (group-n 2 (+ digit)) ":")
+     nil 2
+     ,(cons #'b/ripgrep--col-beg #'b/ripgrep--col-end)
+     2 ;; line number without file -> error
+     )))
 
 (cl-defun b/ripgrep-command (pattern
 			     &key
 			     (case-sensitive 'smart) ;; nil/smart/t
 			     (pattern-type 'fixed) ;; fixed/default/pcre2/auto
 			     follow
-			     heading
+			     (heading t)
 			     search-zip
 			     invert-match
 			     (binary t)
@@ -137,10 +160,20 @@ function properly")
   (setq-local compilation-disable-input t)
   (toggle-truncate-lines t)
 
+  (setq-local compilation-skip-threshold (if b/ripgrep-use-headings
+					     2 ;; only show errors, i.e. files 
+					   0))
   (setq-local compilation-mode-line-errors grep-mode-line-matches)
   (setq-local compilation-process-setup-function #'b/ripgrep-process-setup)
+
   (setq-local compilation-error-face grep-hit-face)
-  (setq-local compilation-error-regexp-alist b/ripgrep-error-regexp-alist)
+  (setq-local compilation-warning-face grep-hit-face)
+
+  (setq-local compilation-error-regexp-alist-alist b/ripgrep-error-regexp-alist-alist)
+  (setq-local compilation-error-regexp-alist
+	      (if b/ripgrep-use-headings
+		  '(rg-file-only rg-binary-file rg-line-only)
+		'(rg-file-line rg-binary-file)))
 
   (unless kill-transform-function
     (setq-local kill-transform-function #'identity))
@@ -155,7 +188,7 @@ function properly")
   (interactive
    (let ((pattern (read-from-minibuffer "Ripgrep: " nil nil nil 'b/ripgrep-history)))
      (list pattern)))
-  (compilation-start (b/ripgrep-command needle)
+  (compilation-start (b/ripgrep-command needle :heading b/ripgrep-use-headings)
 		     #'b/ripgrep-mode))
 
 (defun b/ripgrep-project (project)
