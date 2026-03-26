b03fd3c75aa79b37ded134022c3881e3912ae0be
Alexey Lebedeff <binarin@binarin.info>
2026-03-26 17:49:52 +0100

    set compile command for files/emacs/ dir

---
+++ files/emacs/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((eval . (setq-local compile-command (format "cd %s && ./scripts/test-emacs.sh"
+					 (project-root (project-current))))))))
