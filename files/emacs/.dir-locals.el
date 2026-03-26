34171323847f357c1395f5c69bd3dde6240e1e87
Alexey Lebedeff <binarin@binarin.info>
2026-03-26 17:53:02 +0100

    emacs: funnily enough, compile-command is already eval'ed

--- files/emacs/.dir-locals.el
+++ files/emacs/.dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((emacs-lisp-mode . ((eval . (setq-local compile-command (format "cd %s && ./scripts/test-emacs.sh"
-					 (project-root (project-current))))))))
+((emacs-lisp-mode . ((compile-command . (format "cd %s && ./scripts/test-emacs.sh"
+					        (project-root (project-current)))))))
