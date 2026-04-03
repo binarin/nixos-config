4559f509b5563ab98e2cfaa19ec7b5aa489ae955
Alexey Lebedeff <binarin@binarin.info>
2026-04-03 15:57:11 +0200

    niri-dynamic-keybindings: add overlay, refactor to shared packageFn

--- modules/packages/niri-dynamic-keybindings.nix
+++ modules/packages/niri-dynamic-keybindings.nix
@@ -2,22 +2,29 @@
 let
   selfLib = self.lib.self;
   niriCargo = builtins.fromTOML (selfLib.read' "packages/niri-dynamic-keybindings/Cargo.toml");
+  packageFn =
+    { rustPlatform, lib }:
+    rustPlatform.buildRustPackage {
+      pname = "niri-dynamic-keybindings";
+      version = "0.1.0";
+      src = selfLib.dir' "packages/niri-dynamic-keybindings";
+      cargoLock = {
+        lockFile = selfLib.file' "packages/niri-dynamic-keybindings/Cargo.lock";
+        outputHashes."niri-ipc-25.11.0" = niriCargo.dependencies.niri-ipc.ipcHash;
+      };
+      meta.mainProgram = "niri-dynamic-keybindings";
+    };
 in
 {
   perSystem =
     { pkgs, lib, ... }:
     {
-      packages.niri-dynamic-keybindings = pkgs.rustPlatform.buildRustPackage {
-        pname = "niri-dynamic-keybindings";
-        version = "0.1.0";
-        src = selfLib.dir' "packages/niri-dynamic-keybindings";
-        cargoLock = {
-          lockFile = selfLib.file' "packages/niri-dynamic-keybindings/Cargo.lock";
-          outputHashes."niri-ipc-25.11.0" = niriCargo.dependencies.niri-ipc.ipcHash;
-        };
-        meta.mainProgram = "niri-dynamic-keybindings";
-      };
+      packages.niri-dynamic-keybindings = pkgs.callPackage packageFn { };
     };
+
+  flake.overlays.niri-dynamic-keybindings = final: prev: {
+    niri-dynamic-keybindings = final.callPackage packageFn { };
+  };
 
   flake.homeModules.niri-dynamic-keybindings =
     {
