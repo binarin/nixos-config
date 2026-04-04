f1c02dad6410afcd5f56fa96967c4b4837147a8e
Alexey Lebedeff <binarin@binarin.info>
2026-04-04 17:22:16 +0200

    emacs: decrese mode-line font size uncodiitionally
    there is something strange about 'display-graphic-p

--- files/emacs/user-lisp/b-visual.el
+++ files/emacs/user-lisp/b-visual.el
@@ -17,9 +17,8 @@
 (load-theme 'zenburn t)
 (add-to-list 'default-frame-alist '(font . "IosevkaTerm Nerd Font-16"))
 
-(when (display-graphic-p)
-  (set-face-attribute 'mode-line nil :height 0.8)
-  (set-face-attribute 'mode-line-inactive nil :height 0.8))
+(set-face-attribute 'mode-line nil :height 0.8)
+(set-face-attribute 'mode-line-inactive nil :height 0.8)
 (line-number-mode 1)
 (column-number-mode 1)
 
