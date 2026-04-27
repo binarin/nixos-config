aea1cd381d0ff0f09b9f97f61f8d0d5a05ea6118
Automatic Flake Updater <flake-updater@binarin.info>
2026-04-27 12:15:34 +0200

    Bump nixpkgs-trezor-agent to match nixpkgs

--- flake.nix
+++ flake.nix
@@ -104,7 +104,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-trezor-agent.url = "github:nixos/nixpkgs?rev=41e216c0ca66c83b12ab7a98cc326b5db01db646";
+    nixpkgs-trezor-agent.url = "github:nixos/nixpkgs?rev=daf6dc47aa4b44791372d6139ab7b25269184d55";
     nixpkgs-unstable.url = "github:nixos/nixpkgs";
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     sops-nix = {
