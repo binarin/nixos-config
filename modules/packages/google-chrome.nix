67092a78404c10794755ab756dccbb7ec908f302
Alexey Lebedeff <binarin@binarin.info>
2026-04-28 12:04:39 +0200

    google-chrome proxy enforcement: fix .desktop files to use wrapped binary

--- modules/packages/google-chrome.nix
+++ modules/packages/google-chrome.nix
@@ -6,12 +6,19 @@
         chromeWrapper = final.writeShellScriptBin "google-chrome-stable" ''
           exec ${lib.getExe prev.google-chrome} --proxy-server=socks5://localhost:3128 "$@"
         '';
+
+        desktopFiles = final.runCommand "google-chrome-desktop-files" { } ''
+          mkdir -p "$out/share/applications"
+          for f in ${prev.google-chrome}/share/applications/*.desktop; do
+            sed "s|/nix/store/[^-]*-google-chrome-[^/]*/bin/google-chrome-stable|${chromeWrapper}/bin/google-chrome-stable|g" "$f" > "$out/share/applications/$(basename "$f")"
+          done
+        '';
       in
-      final.buildEnv {
+      final.symlinkJoin {
         name = prev.google-chrome.name;
-        ignoreCollisions = true;
         paths = [
           chromeWrapper
+          desktopFiles
           prev.google-chrome
         ];
         meta = prev.google-chrome.meta // { mainProgram = "google-chrome-stable"; };
