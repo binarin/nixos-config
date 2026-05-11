c444ade5ff03e7cac158de5428a27cd62cb7637c
Alexey Lebedeff <binarin@binarin.info>
2026-05-11 20:06:31 +0200

    26.05 preparation - let's try to build from master

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
       url = "github:nix-community/NixOS-WSL/main";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs";
     nixpkgs-unstable.url = "github:nixos/nixpkgs";
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     sops-nix = {
@@ -123,9 +123,5 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix.url = "github:numtide/treefmt-nix";
-    waybar = {
-      url = "github:Alexays/Waybar";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
   };
 }
