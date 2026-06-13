0730603e771f4bf34c99cf2533956c24419bcdd4
Alexey Lebedeff <binarin@binarin.info>
2026-06-13 16:50:41 +0200

    fix(emacs): replace obsolete rx  with  in b-org.el

--- files/emacs/user-lisp/b-org.el
+++ files/emacs/user-lisp/b-org.el
@@ -111,7 +111,7 @@
   (interactive)
   (org-capture-string (format "(b/ripgrep-main %s)" (prin1-to-string (b/active-region-or-symbol-at-point))) "Z"))
 
-(defvar b/murmur-home-path (if (string-match-p (rx bos "LL" (* any) "44" eos) (system-name))
+(defvar b/murmur-home-path (if (string-match-p (rx bos "LL" (* nonl) "44" eos) (system-name))
                                (expand-file-name "~")
                              (pcase (system-name)
                                ("murmur" (expand-file-name "~"))
