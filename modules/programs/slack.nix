760ca7349bc1882ee0fef94fea2eff3993de7224
Alexey Lebedeff <binarin@binarin.info>
2026-07-17 12:24:50 +0200

    feat(shitty-shit): route Slack through wrapShittyShit

--- modules/programs/slack.nix
+++ modules/programs/slack.nix
@@ -1,12 +1,20 @@
 { self, ... }:
 {
   flake.overlays.slack = final: prev: {
-    slack = prev.slack.overrideAttrs (old: {
-      installPhase = old.installPhase + ''
-        local icons="$out/lib/slack/resources/app.asar.unpacked/dist/resources"
-        cp "$icons/slack-taskbar-rest.ico" "$icons/slack-taskbar-unread.ico"
-        cp "$icons/slack-taskbar-rest.ico" "$icons/slack-taskbar-highlight.ico"
-      '';
-    });
+    slack =
+      let
+        slackWithIcons = prev.slack.overrideAttrs (old: {
+          installPhase = old.installPhase + ''
+            local icons="$out/lib/slack/resources/app.asar.unpacked/dist/resources"
+            cp "$icons/slack-taskbar-rest.ico" "$icons/slack-taskbar-unread.ico"
+            cp "$icons/slack-taskbar-rest.ico" "$icons/slack-taskbar-highlight.ico"
+          '';
+        });
+      in
+      final.wrapShittyShit slackWithIcons {
+        talk = [ ];
+        own = [ ];
+        see = [ ];
+      };
   };
 }
