89ff9e2d3e6136d725f649af4b6c665e8bf734d5
Alexey Lebedeff <binarin@binarin.info>
2026-07-31 22:11:13 +0200

    emacs: workaround for some emacs bugs
    still need to report:
    - compile-command defcustom is limited to 'string
    - there is double format %s interpretation in warnings somewhere

--- files/emacs/.dir-locals.el
+++ files/emacs/.dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
-
-((emacs-lisp-mode . ((compile-command . (concat "cd " (project-root (project-current)) " && ./scripts/test-emacs.sh")))))
+((emacs-lisp-mode . ((eval . (setq-local compile-command (format "cd %s && ./scripts/test-emacs.sh"
+					                         (project-root (project-current))))))))
